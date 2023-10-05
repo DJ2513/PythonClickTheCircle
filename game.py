810c78a1582ab3ac79b7f080bde7b8095703e10d
DJ2513 <djontiveros2728@gmail.com>
2023-10-04 20:41:33 -0600

    finalizando el juego

--- game.py
+++ game.py
@@ -21,35 +21,32 @@ def checkCollition() -> bool:
         return True
     return False
 
-# def checkNotCollition() -> bool:
-#     if checkCollition() == False:
-#         lives-=1
-#         if lives == 0:
-#             screen.fill("red")
-#             score = "You lose!"
-
-
-while True:
+running = True
+while running:
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:
+            running
             pygame.quit()
             sys.exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:  # left click
                 if checkCollition():
                     score += 1
-                    circlePos = (random.randint(0, 1280),
-                                 random.randint(0, 720))
+                    circlePos = (random.randint(0, 1280), random.randint(0, 720))
                 else:
                     lives -= 1
-                    if lives == 0:
-                        screen.fill("red")
 
     score_label = font.render(f'Score: {score}', True, "black")
     lives_label = font.render(f'Lives:{lives} ', True, "black")
+    overalScore_label = font.render(f'Your score was: {score} points!', True, 'white')
+    gameOver_label = font.render(f'GAME OVER!', True, 'white')
     screen.fill('lightblue')
     pygame.draw.circle(screen, "red", circlePos, 50)
     screen.blit(score_label, (25, 25))
     screen.blit(lives_label, (1100, 25))
+    if lives <= 0:
+        screen.fill('black')
+        screen.blit(gameOver_label, (570, 350))
+        screen.blit(overalScore_label, (520, 400))
     pygame.display.update()
